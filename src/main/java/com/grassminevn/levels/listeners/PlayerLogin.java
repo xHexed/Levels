6afe88bb30b6388aa7b2950cabedba45ff24a77c
xHexed <minhquanpct7b@gmail.com>
2020-11-21 21:54:56 +0800

    Prevent inserting kicked players

--- src/main/java/com/grassminevn/levels/listeners/PlayerLogin.java
+++ src/main/java/com/grassminevn/levels/listeners/PlayerLogin.java
@@ -2,7 +2,6 @@ package com.grassminevn.levels.listeners;
 
 import com.grassminevn.levels.Levels;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 
@@ -13,8 +12,11 @@ public class PlayerLogin implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    @EventHandler(ignoreCancelled = true)
     public void onLogin(final PlayerLoginEvent e) {
+        if (e.getResult() != PlayerLoginEvent.Result.ALLOWED) {
+            return;
+        }
         plugin.database.insert(e.getPlayer().getUniqueId());
     }
 }
